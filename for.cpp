42cbe3b1b47c5c5758d054d59d56e982a5d88faa
Angel4091 <00048825@uca.edu.sv>
2025-06-05 20:16:27 -0600

    :fire:

---
+++ for.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int b = 10;
+    
+
+    for(int i = 10; i>=1;i--){
+        cout << i << "x" << b << "=" << i * b << endl;
+
+    }
+
+    return 0;
+}
